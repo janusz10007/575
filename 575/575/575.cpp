32d420c0e5d56729748c5823fd6994079a91adee
janusz10007 <48623077+janusz10007@users.noreply.github.com>
2020-11-26 02:06:25 +0100

    done

---
+++ 575/575/575.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+
+bool tab[100000];
+
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    const int SIZE = 1000000010;
+    vector <int> primenmbs;
+
+    for (int i = 2; i <= sqrt(SIZE); i++) {
+        if (tab[i]) continue;
+        primenmbs.push_back(i * i);
+        for (int j = 2 * i; j <= sqrt(SIZE); j += i)
+            tab[j] = true;
+    }
+
+    int a, b, t;
+
+    cin >> t;
+
+    while (t--) {
+        cin >> a >> b;
+        int zlicz = 0;
+        for (int i = 0; i < primenmbs.size(); i++) {
+            if (primenmbs[i] >= a && primenmbs[i] <= b)
+                zlicz++;
+            if (primenmbs[i] > b)
+                break;
+        }
+        cout << zlicz << endl;
+    }
+}
